d9a09fee435ad20fe0dca96ae22f2578ce66cbfe
Đinh Thị Lan Anh <179322689+lananhdt@users.noreply.github.com>
2025-08-25 22:31:43 +0700

    app.py

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ st.session_state.page = choice
 
 def goto(page_name:str):
     st.session_state.page = page_name
-    st.rerun()
 
 # ================== DATA ==================
 DATA_PATH = "data/emails.csv"
